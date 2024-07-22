889d988474a85c79ff0ec390bdf7a916fe50ae9b
NicoD-SBC <46458369+NicoD-SBC@users.noreply.github.com>
2024-07-22 18:55:12 +0200

    Update reicast-linux.sh

--- reicast-linux.sh
+++ reicast-linux.sh
@@ -1,1 +1,19 @@
+# Install prerequisites
+sudo apt-get -y install build-essential libasound2 libasound2-dev libegl1-mesa-dev libgl1-mesa-dev libgles2-mesa-dev mesa-common-dev
+# Build Reicast
+git clone https://github.com/reicast/reicast-emulator.git
+cd reicast-emulator/reicast/linux
+make
 
+# Run Reicast
+mkdir -p ~/.local/share/reicast/data/
+cp ../android-studio/reicast/src/main/assets/buttons.png ~/.local/share/reicast/data/
+
+echo "Open with" 
+echo "./reicast.elf"
+
+
+echo "To open a ROM"
+echo "./reicast.elf /path/to/rom.cdi"
+
+echo "Copy BIOS to ~/.reicast/data"
